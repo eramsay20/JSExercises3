ce6f2c7a6fe2ca840595ab63d2194ac09d7c8ede
Eric Ramsay <64610054+eramsay20@users.noreply.github.com>
2020-05-19 16:21:24 -0400

    created rand color generator

--- exercise-three.js
+++ exercise-three.js
@@ -13,7 +13,37 @@ generateNum();
 
 /*
 3. Create a JavaScript function called generateColor that uses a random number generator
-to produce a number from 1 to 6. Inside this function, each value should map to a different color. For example, 1 = yellow, 2 = red, etc. The function takes no parameters and returns a color string.
+to produce a number from 1 to 6. Inside this function, each value should map to a different 
+color. For example, 1 = yellow, 2 = red, etc. The function takes no parameters and returns 
+a color string.
+*/
+
+function generateColor() {
+    colorSelect = Math.floor((Math.random() * 6) + 1); // generate a num assigned to a color
+    switch (colorSelect) {
+        case 1:
+        return "Red";
+        break;
+        case 2:
+        return "Orange";
+        break;
+        case 3:
+        return "Yellow";
+        break;
+        case 4:
+        return "Green";
+        break;
+        case 5:
+        return "Blue";
+        break;
+        case 6:
+        return "Purple";
+        break;
+    }
+}
+generateColor();
+
+/*
 4. Create a function that creates and returns car objects. Car objects should have a field that holds a string for their color. This function should utilize generateColor to assign a random color to each car that is created. This function should also take an input parameter for the productionRun, which is added as a field to the returned car.
 5. Create a JavaScript function called constructCars. This function should take in two parameters: a number, and a string production number. This function should use the number input and the car creating function from requirement #4 to build that many car objects and store them in an array. Each car built with this function should be assigned the same production run from the input parameter. This function should return an array of car objects.
 6. Create a JavaScript function called displayAllCars that takes an array as input and does not return anything. This function should iterate through the array and display each element on the console.
